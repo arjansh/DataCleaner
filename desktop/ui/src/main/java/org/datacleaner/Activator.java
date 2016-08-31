953b3696b845c609695df085e8a99eb85ca96303
Arjan Seijkens <Arjan.Seijkens@humaninference.com>
2016-08-31 08:15:09 +0200

    First commit which makes it possible to run datacleaner desktop ui as a bundle in osgi (tested with Felix). It starts and opens DataCleaner desktop. Doesn't fully work yet, because it fails to load the default components.

---
+++ desktop/ui/src/main/java/org/datacleaner/Activator.java
@@ -0,0 +1,37 @@
+/**
+ * DataCleaner (community edition)
+ * Copyright (C) 2014 Neopost - Customer Information Management
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.datacleaner;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		Main.main(new String[] {});
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		// Do nothing.
+	}
+
+}
